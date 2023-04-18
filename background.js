06fd08da5ee742c16e9af840408e4f387867f549
Yi Ming Yung <yiming.yung@outlook.com>
2023-04-18 16:30:40 +0200

    Bugfix: Only addressbar urls should be changed

--- background.js
+++ background.js
@@ -18,29 +18,31 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
 
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
-        let redirectUrl;
+        if (details.type === 'main_frame') {
+            let redirectUrl;
 
-        if (
-            redirectOption === 'vgToVoys' &&
-            details.url.startsWith('https://partner.voipgrid.nl')
-        ) {
-            redirectUrl = details.url.replace(
-                'https://partner.voipgrid.nl',
-                'https://freedom.voys.nl'
-            );
-        } else if (
-            redirectOption === 'voysToVg' &&
-            details.url.startsWith('https://freedom.voys.nl')
-        ) {
-            redirectUrl = details.url.replace(
-                'https://freedom.voys.nl',
-                'https://partner.voipgrid.nl'
-            );
-        }
+            if (
+                redirectOption === 'vgToVoys' &&
+                details.url.startsWith('https://partner.voipgrid.nl')
+            ) {
+                redirectUrl = details.url.replace(
+                    'https://partner.voipgrid.nl',
+                    'https://freedom.voys.nl'
+                );
+            } else if (
+                redirectOption === 'voysToVg' &&
+                details.url.startsWith('https://freedom.voys.nl')
+            ) {
+                redirectUrl = details.url.replace(
+                    'https://freedom.voys.nl',
+                    'https://partner.voipgrid.nl'
+                );
+            }
 
-        if (redirectUrl) {
-            chrome.tabs.update(details.tabId, { url: redirectUrl });
-            return { cancel: true };
+            if (redirectUrl) {
+                chrome.tabs.update(details.tabId, { url: redirectUrl });
+                return { cancel: true };
+            }
         }
     },
     { urls: ['*://partner.voipgrid.nl/*', '*://freedom.voys.nl/*'] }
